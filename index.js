66982c18c668478ab9a4ca2c8654083430e24c68
Andreizayaz <poznavauka@mail.ru>
2021-02-27 14:22:47 +0300

    feat: create adaptive layout and refactor addTag and init features in index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 'use strict'
 
-const container = document.querySelector('.container');
 const textFromInput = document.querySelector('.text-input');
 const btnSave = document.querySelector('.btn-save');
 const readOnlySwitchMode = document.querySelector('#readonlySwitchMode');
 const tagList = document.querySelector('.tag-list');
 
-let tagItemsList=[];
+let tagItemsList;
 
 initTagList();
 
 btnSave.addEventListener('click', addTag);
-readOnlySwitchMode.addEventListener('click', switchReadonlyMode);
+readOnlySwitchMode.addEventListener('click', () => btnSave.disabled = !btnSave.disabled);
 tagList.addEventListener('click', deleteTagItem)
 
 function addTag() {
@@ -21,17 +20,16 @@ function addTag() {
     textFromInput.value = '';
     tagItemsList.push(tagName);
     localStorage.setItem('tags', JSON.stringify(tagItemsList));
+  } else {
+    alert('You entered empty string!');
+    textFromInput.value = '';
   }
-  return;
 }
 
-function switchReadonlyMode() {
-  btnSave.disabled = !btnSave.disabled;
-}
 
 function deleteTagItem(e) {
-  if ((e.target.tagName === 'BUTTON'
-  || e.target.closest('button')) && !readOnlySwitchMode.checked) {
+  if (!readOnlySwitchMode.checked && (e.target.tagName === 'BUTTON'
+  || e.target.closest('button'))) {
     const tagItem = e.target.closest('li');
     tagItem.remove();
     tagItemsList.splice(tagItemsList.indexOf(tagItem.innerText), 1);
@@ -40,8 +38,8 @@ function deleteTagItem(e) {
 }
 
 function initTagList() {
-  tagItemsList = JSON.parse(localStorage.getItem('tags'));
-  if (tagItemsList.length>0) {
+  tagItemsList = JSON.parse(localStorage.getItem('tags'))||[];
+  if (tagItemsList && tagItemsList.length > 0) {
     tagItemsList.forEach(item => tagList.insertAdjacentHTML('afterbegin', `<li class="tag-item"><p class="tag-text">${item}</p><button class="btn-delete"><img class="btn-delete-icon" src="images/delete.png" alt="delete"></button></li>`))
   } else {
     alert('There are not tags!');
